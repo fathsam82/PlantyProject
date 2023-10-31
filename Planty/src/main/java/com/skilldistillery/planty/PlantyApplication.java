1579ac457a8f50734ec6c8dbb77e24392e2f5e87
fathsam82 <fathsam82@gmail.com>
2023-10-31 13:23:32 -0600

    Create Spring Boot REST, linked JPA project dependency

---
+++ Planty/src/main/java/com/skilldistillery/planty/PlantyApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.planty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantyApplication.class, args);
+	}
+
+}
---
+++ Planty/src/test/java/com/skilldistillery/planty/PlantyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.planty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
