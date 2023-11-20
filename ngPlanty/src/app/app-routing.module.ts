bc820b36325972ed751ce2b12295572924b89389
fathsam82 <fathsam82@gmail.com>
2023-11-20 15:05:13 -0700

    added orderDetail & orderCart models to angular project

--- ngPlanty/src/app/app-routing.module.ts
+++ ngPlanty/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlantListComponent } from './components/plant-list/plant-list.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes =  [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent},
+  { path: 'plantList', component: PlantListComponent}
+  // { path: '**', component: NotFoundComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ngPlanty/src/app/components/plant-list/plant-list.component.ts
+++ ngPlanty/src/app/components/plant-list/plant-list.component.ts
@@ -1,35 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PlantService } from 'src/app/services/plant.service';
 import { Plant } from 'src/app/models/plant';
 
 @Component({
   selector: 'app-plant-list',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './plant-list.component.html',
-  styleUrl: './plant-list.component.css'
+  styleUrl: './plant-list.component.css',
 })
 export class PlantListComponent implements OnInit {
-
-  title = "ngPlant";
+  title = 'ngPlant';
 
   plants: Plant[] = [];
 
   selected: Plant | null = null;
 
-
-
-  constructor(private plantService: PlantService){
-
-  }
+  constructor(private plantService: PlantService) {}
 
   ngOnInit() {
     this.reload();
-
   }
 
-  reload(){
+  reload() {
     this.plantService.index().subscribe({
       next: (plantList) => {
         this.plants = plantList;
@@ -37,11 +28,7 @@ export class PlantListComponent implements OnInit {
       error: (somethingBad) => {
         console.error('PlantListComponent.reload: error loading plants');
         console.error(somethingBad);
-      }
+      },
     });
-
-
-
   }
-
 }
