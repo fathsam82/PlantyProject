855f5b17158d8f709c4fede33e7ff55432de8416
fathsam82 <fathsam82@gmail.com>
2023-12-01 12:24:18 -0700

    constructed orderCart model

--- ngPlanty/src/app/models/order-cart.ts
+++ ngPlanty/src/app/models/order-cart.ts
@@ -1,2 +1,43 @@
+import { User } from "./user";
+// import { PaymentData } from "./payment-data";
+
+
 export class OrderCart {
+  id: number;
+  totalPrice: number;
+  datePlaced: string;
+  notes: string;
+  estimatedDeliveryDate: string;
+  trackingNumber: number;
+  paymentMethod: string;
+  enabled: boolean;
+  user: User;
+  paymentData: PaymentData;
+
+
+  constructor(
+id: number = 0,
+totalPrice: number = 0,
+datePlaced: string = '',
+notes: string = '',
+estimatedDeliveryDate: string = '',
+trackingNumber: number = 0,
+paymentMethod: string = '',
+enabled: boolean = false,
+user: User = new User(),
+paymentData: PaymentData = new PaymentData()
+  ){
+    this.id = id;
+    this.totalPrice = totalPrice;
+    this.datePlaced = datePlaced;
+    this.notes = notes;
+    this.estimatedDeliveryDate = estimatedDeliveryDate;
+    this.trackingNumber = trackingNumber;
+    this.paymentMethod = paymentMethod;
+    this.enabled = enabled;
+    this.user = user;
+    this.paymentData = paymentData;
+
+  }
+
 }
