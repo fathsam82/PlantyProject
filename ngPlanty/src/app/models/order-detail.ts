078b795d5d5e5f2026ec35471b97880646003d3a
fathsam82 <fathsam82@gmail.com>
2023-12-01 12:10:54 -0700

    constructed orderDetail model

--- ngPlanty/src/app/models/order-detail.ts
+++ ngPlanty/src/app/models/order-detail.ts
@@ -1,7 +1,32 @@
+import { Plant } from './plant';
+import { OrderCart } from "./order-cart";
+
 export class OrderDetail {
 
-  // id: number;
-  // quantityOrdered: number;
-  // unitPrice: number;
+  id: number;
+  quantityOrdered: number;
+  unitPrice: number;
+  subtotalPrice: number;
+  giftWrap: boolean;
+  orderCart: OrderCart;
+  plant: Plant;
 
+  constructor(
+    id: number = 0,
+    quantityOrdered: number = 0,
+    unitPrice: number = 0,
+    subtotalPrice: number = 0,
+    giftWrap: boolean = false,
+    orderCart: OrderCart = new OrderCart(),
+    plant: Plant = new Plant()
+  ){
+  this.id = id;
+  this.quantityOrdered = quantityOrdered;
+  this.unitPrice = unitPrice;
+  this.subtotalPrice = subtotalPrice;
+  this.giftWrap = giftWrap;
+  this.orderCart = orderCart;
+  this.plant = plant;
+
+  }
 }
