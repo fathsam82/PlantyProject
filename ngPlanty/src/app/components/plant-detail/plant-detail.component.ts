f795b94f2b25701ab40f506199fd915a62931a04
fathsam82 <fathsam82@gmail.com>
2024-01-10 12:15:33 -0700

    generated plantDetail component

---
+++ ngPlanty/src/app/components/plant-detail/plant-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-plant-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './plant-detail.component.html',
+  styleUrl: './plant-detail.component.css'
+})
+export class PlantDetailComponent {
+
+}
