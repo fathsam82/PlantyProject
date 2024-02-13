35eda5468995b14480f6e0ee86025abc44031cf5
fathsam82 <fathsam82@gmail.com>
2024-02-13 13:35:20 -0700

    injected orderDetail constructor with activatedRoute & constructed get function in orderDetail component.ts

--- ngPlanty/src/app/components/order-detail/order-detail.component.ts
+++ ngPlanty/src/app/components/order-detail/order-detail.component.ts
@@ -1,16 +1,35 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { OrderDetailService } from 'src/app/services/order-detail.service';
+import { OrderDetail } from 'src/app/models/order-detail';
 
 @Component({
   selector: 'app-order-detail',
   templateUrl: './order-detail.component.html',
-  styleUrl: './order-detail.component.css'
+  styleUrl: './order-detail.component.css',
 })
 export class OrderDetailComponent {
+  orderDetail: OrderDetail | undefined;
 
-  constructor(private orderDetailService: OrderDetailService){
+  constructor(
+    private orderDetailService: OrderDetailService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-
+  getOrderDetail() {
+    const orderDetailId = this.activatedRoute.snapshot.paramMap.get('id');
+    if (orderDetailId !== null) {
+      const id = Number(orderDetailId);
+      if (!isNaN(id)) {
+        this.orderDetailService.getOrderDetail(id).subscribe({
+          next: (orderDetail) => (this.orderDetail = orderDetail),
+          error: (err) => console.error(err),
+        });
+      } else {
+        console.error('Invalid orderDetail ID:', orderDetailId);
+      }
+    } else {
+      console.error('OrderDetail ID is missing the route parameters');
+    }
   }
-
 }
