ac1bee217cb7829a501ec84f21fce15e59b3705b
fathsam82 <fathsam82@gmail.com>
2024-02-21 11:38:19 -0700

    generated orderCart component via Angular Cli

---
+++ ngPlanty/src/app/components/order-cart/order-cart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-order-cart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './order-cart.component.html',
+  styleUrl: './order-cart.component.css'
+})
+export class OrderCartComponent {
+
+}
