053e508e73c1bdc956f11e59822ac56a0a864b90
fathsam82 <fathsam82@gmail.com>
2024-03-19 10:45:52 -0600

    created JPA entity shippingAddress w/ 1to1 relationship with user

---
+++ JPAPlanty/src/main/java/com/skilldistillery/planty/entities/ShippingAddress.java
@@ -0,0 +1,112 @@
+package com.skilldistillery.planty.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class ShippingAddress {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "street_address")
+	private String streetAddress;
+	
+	private String zipcode;
+	
+	private String city;
+	
+	private String state;
+	
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	private User user; 
+	
+	public ShippingAddress() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ShippingAddress other = (ShippingAddress) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "ShippingAddress [id=" + id + ", streetAddress=" + streetAddress + ", zipcode=" + zipcode + ", city="
+				+ city + ", state=" + state + ", user=" + user + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ JPAPlanty/src/test/java/com/skilldistillery/planty/entities/ShippingAddressTest.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.planty.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShippingAddressTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
