1748f1ef6835d2fa45dc0f2df2c8f3d242017154
fathsam82 <fathsam82@gmail.com>
2024-03-25 13:53:47 -0600

    constructed paymentData.service.ts getByUsername function

--- ngPlanty/src/app/services/payment-data.service.ts
+++ ngPlanty/src/app/services/payment-data.service.ts
@@ -1,9 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
+import { PaymentData } from '../models/payment-data';
+import { Observable, catchError, switchMap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDataService {
+  private url = environment.baseUrl + 'api/paymentData';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  getPaymentDataByUsername(username: string): Observable<PaymentData> {
+    return this.authService.getLoggedInUser().pipe(
+      switchMap((user) => {
+        if(!user) {
+          throw new Error('User not logged in');
+        }
+        const urlWithUsername = `${this.url}/${username}`;
+        return this.httpClient.get<PaymentData>(urlWithUsername, this.getHttpOptions()).pipe(
+          catchError((err: any) => {
+            console.log(err);
+            return throwError(
+              () =>
+              new Error('PaymentDataService.getPaymentDataByUsername(): error retrieving paymentData')
+            );
+          })
+        );
+      })
+    )
+  }
+
 }
