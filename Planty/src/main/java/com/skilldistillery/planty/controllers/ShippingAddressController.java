3bf32b29fd9c2073303ad3b4437e61d2435a0377
fathsam82 <fathsam82@gmail.com>
2024-04-09 09:45:38 -0600

    constructed controller method for shippingAddress w/ parameters principal and shippingAddressId

--- Planty/src/main/java/com/skilldistillery/planty/controllers/ShippingAddressController.java
+++ Planty/src/main/java/com/skilldistillery/planty/controllers/ShippingAddressController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +42,23 @@ public class ShippingAddressController {
 			res.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+	}
+	
+	@GetMapping("shippingAddress/{shippingAddressId}")
+	public ResponseEntity<ShippingAddress> getShippingAddress(@PathVariable int shippingAddressId, Principal principal, HttpServletResponse res){
+		if(principal == null) {
+			res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+		try {
+			String username = principal.getName();
+			ShippingAddress shippingAddress = shippingAddressService.getShippingAddress(shippingAddressId, username);
+			return ResponseEntity.ok(shippingAddress);
+		} catch (EntityNotFoundException e) {
+			res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		
 	}
 
 }
