912bd484ec9f095e20b454812a8139dbd2d6d0f4
fathsam82 <fathsam82@gmail.com>
2024-04-29 13:35:33 -0600

    constructed get endpoint in plantOrigin controller & successfully tested via postman

---
+++ Planty/src/main/java/com/skilldistillery/planty/controllers/PlantOriginController.java
@@ -0,0 +1,42 @@
+package com.skilldistillery.planty.controllers;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.planty.entities.PlantOrigin;
+import com.skilldistillery.planty.services.PlantOriginService;
+
+@RestController
+@CrossOrigin({ "*", "http://localhost/" })
+@RequestMapping("api")
+public class PlantOriginController {
+	
+	
+	@Autowired
+	private PlantOriginService plantOriginService;
+	@GetMapping("plants/{id}/origin")
+	public ResponseEntity<?> getPlantOriginByPlantId(@PathVariable("id") int plantId) {
+	    try {
+	        PlantOrigin plantOrigin = plantOriginService.getPlantOriginByPlantId(plantId);
+	        if (plantOrigin != null) {
+	            return ResponseEntity.ok(plantOrigin);
+	        } else {
+	            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Origin not found for plant with id: " + plantId + " 🌍");
+	        }
+	    } catch (EntityNotFoundException e) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Crikey! An error occurred while fetching the plant's origin 😖");
+	    }
+	}
+
+}
