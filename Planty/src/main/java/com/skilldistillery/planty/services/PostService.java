e0d55ddcce7010b845e6f2fb46013feb866045db
fathsam82 <fathsam82@gmail.com>
2024-09-17 12:20:02 -0600

    created service interface & constructed service method for implementation 'listAllPosts'

---
+++ Planty/src/main/java/com/skilldistillery/planty/services/PostService.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.planty.services;
+
+import java.util.List;
+
+import com.skilldistillery.planty.entities.Post;
+
+public interface PostService {
+	
+	List<Post> listAllPosts();
+
+}
