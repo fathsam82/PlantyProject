fb62a17e3f47737e981f8dcb2d64dc00fd64b239
fathsam82 <fathsam82@gmail.com>
2024-09-17 12:27:07 -0600

    Autowired in PostRepository to PostServiceImpl class

--- Planty/src/main/java/com/skilldistillery/planty/services/PostServiceImpl.java
+++ Planty/src/main/java/com/skilldistillery/planty/services/PostServiceImpl.java
@@ -2,12 +2,19 @@ package com.skilldistillery.planty.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.skilldistillery.planty.entities.Post;
+import com.skilldistillery.planty.repositories.PostRepository;
 
 public class PostServiceImpl implements PostService {
+	
+	@Autowired
+	private PostRepository postRepo;
 
 	@Override
 	public List<Post> listAllPosts() {
+		
 		return null;
 	}
 
