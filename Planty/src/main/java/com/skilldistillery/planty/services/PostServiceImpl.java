202fbac6ecbc95d067f07418d46d4813a553ca1a
fathsam82 <fathsam82@gmail.com>
2024-09-17 12:24:12 -0600

    created PostImpl class to implement inherited abstract methods from PostService interface 'listAllPosts'

---
+++ Planty/src/main/java/com/skilldistillery/planty/services/PostServiceImpl.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.planty.services;
+
+import java.util.List;
+
+import com.skilldistillery.planty.entities.Post;
+
+public class PostServiceImpl implements PostService {
+
+	@Override
+	public List<Post> listAllPosts() {
+		return null;
+	}
+
+}
