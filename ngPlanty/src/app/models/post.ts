f918ac4a9f272746f83074393901959299ec38cb
fathsam82 <fathsam82@gmail.com>
2024-09-18 10:03:44 -0600

    constructed model for post -front-end

--- ngPlanty/src/app/models/post.ts
+++ ngPlanty/src/app/models/post.ts
@@ -1,2 +1,31 @@
+import { User } from "./user";
+
 export class Post {
+id: number;
+title: string;
+content: string;
+createdAt: string;
+user: User;
+comments: Comment[]|undefined;
+enabled: boolean;
+
+constructor(
+  id: number = 0,
+  title: string = '',
+  content: string = '',
+  createdAt: string = '',
+  user: User = new User(),
+  comments: Comment[] = [],
+  enabled: boolean = true
+
+) {
+  this.id = id;
+  this.title = title;
+  this.content = content;
+  this.createdAt = createdAt;
+  this.user = user;
+  this.comments = comments;
+  this.enabled = enabled;
+}
+
 }
