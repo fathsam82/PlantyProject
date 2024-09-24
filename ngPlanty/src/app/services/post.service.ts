a1bd0d442d6e931dc0bcda6df5d80ada7d2f68fc
fathsam82 <fathsam82@gmail.com>
2024-09-24 10:22:04 -0600

    imported environment url to post service

--- ngPlanty/src/app/services/post.service.ts
+++ ngPlanty/src/app/services/post.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+
+private url = environment.baseUrl + 'api/post';
 
   constructor() { }
 }
