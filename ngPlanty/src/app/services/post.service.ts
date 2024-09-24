a65072ba47ff189ca0802c61f70614abf2d4b393
fathsam82 <fathsam82@gmail.com>
2024-09-24 10:50:10 -0600

    constructed get service function to retrieve posts

--- ngPlanty/src/app/services/post.service.ts
+++ ngPlanty/src/app/services/post.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { AuthService } from './auth.service';
+import { Post } from '../models/post';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,4 +15,47 @@ export class PostService {
     private httpClient: HttpClient,
     private authService: AuthService
   ) {}
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  index(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>(this.url, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('PostService.create(): error creating posts: ' + err)
+        );
+      })
+    );
+  }
+
+  // createPost(post: Post): Observable<Post>{
+  //   this.authService.getLoggedInUser().subscribe({
+  //     next: (user) => {
+
+  //     },
+  //     error: (fail) => {
+  //       console.error('ngOnInit(): Error getting user');
+  //       console.error(fail);
+  //     }
+  //   });
+  //   return this.httpClient.post<Post>(this.url, post, this.getHttpOptions()).pipe(
+  //     catchError((err: any) => {
+  //       console.log(err);
+  //       return throwError(
+  //         () => new Error('PostService.createPost(): error creating posts: ' + err)
+  //       );
+  //     })
+  //   );
+  // }
+
+
 }
