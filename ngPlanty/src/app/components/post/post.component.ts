85d84e390379a7d58b8c3d47931954f71ac966b7
fathsam82 <fathsam82@gmail.com>
2024-09-26 08:48:28 -0600

    constructed reload function to get posts in component

--- ngPlanty/src/app/components/post/post.component.ts
+++ ngPlanty/src/app/components/post/post.component.ts
@@ -1,3 +1,4 @@
+import { PostService } from './../../services/post.service';
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/post';
 
@@ -10,7 +11,26 @@ export class PostComponent implements OnInit {
   errorMessage: string = '';
   posts: Post[] | undefined;
 
+
+  constructor(private postService: PostService){
+
+  }
   ngOnInit() {}
 
 
+  reload(){
+    this.errorMessage = '';
+    this.postService.index().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+      },
+      error: (retrievingPostsError) => {
+        console.error('PostComponent.reload: error loading posts');
+        console.error(retrievingPostsError);
+        console.error(this.posts);
+      },
+    });
+  }
+
 }
+
