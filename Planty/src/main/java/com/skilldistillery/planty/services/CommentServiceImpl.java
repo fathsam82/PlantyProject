421f2bfea4ab5611e6016db35a89e7248c93ae00
fathsam82 <fathsam82@gmail.com>
2024-10-14 10:40:09 -0600

    autowired in commentRepository into implementation class to utilize build in method for finding comment for postId

--- Planty/src/main/java/com/skilldistillery/planty/services/CommentServiceImpl.java
+++ Planty/src/main/java/com/skilldistillery/planty/services/CommentServiceImpl.java
@@ -2,15 +2,21 @@ package com.skilldistillery.planty.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.planty.entities.Comment;
+import com.skilldistillery.planty.repositories.CommentRepository;
 
 @Service
 public class CommentServiceImpl implements CommentService{
+	
+	@Autowired
+	private CommentRepository commentRepo;
 
 	@Override
 	public List<Comment> listAllCommentsForPostId(int postId) {
+//		List<Comment> allCommentsForPostId = 
 		return null;
 	}
 
