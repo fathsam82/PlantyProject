5a5328d8b069da1f9e0a4533b61eac8dd2960d15
fathsam82 <fathsam82@gmail.com>
2024-10-28 09:00:07 -0600

    successfully tested commentController get endpoint via postman

--- Planty/src/main/java/com/skilldistillery/planty/controllers/CommentController.java
+++ Planty/src/main/java/com/skilldistillery/planty/controllers/CommentController.java
@@ -1,10 +1,17 @@
 package com.skilldistillery.planty.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.planty.entities.Comment;
 import com.skilldistillery.planty.services.CommentService;
 
 @RestController
@@ -16,6 +23,32 @@ public class CommentController {
 	private CommentService commentService;
 	
 	
-	
-	
+	@GetMapping("comments/{id}")
+	public ResponseEntity<?> getCommentsForPost(@PathVariable("id") int postId){
+		try {
+			List<Comment> comments = commentService.listAllCommentsForPostId(postId);
+			if(comments == null || comments.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No comments found");
+			}
+			else {
+				return ResponseEntity.ok(comments);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occured while fetching comments :(");
+		}
+		
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
