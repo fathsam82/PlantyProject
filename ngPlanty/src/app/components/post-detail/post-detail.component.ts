1b960a7c37196cf0ff9e2477c47df58a5f50bc31
fathsam82 <fathsam82@gmail.com>
2025-01-06 22:29:02 -0700

    injected authService into post-detail constructor

--- ngPlanty/src/app/components/post-detail/post-detail.component.ts
+++ ngPlanty/src/app/components/post-detail/post-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailComponent implements OnInit{
 
-  constructor(){
+  constructor(private authService: AuthService,
+  ){
 
   }
 
