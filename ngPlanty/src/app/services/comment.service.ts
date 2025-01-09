f7381394a9b0f12537840410da6b637ef1ce3293
fathsam82 <fathsam82@gmail.com>
2025-01-08 23:31:39 -0700

    added environment variable in comment service

--- ngPlanty/src/app/services/comment.service.ts
+++ ngPlanty/src/app/services/comment.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  private url = environment.baseUrl + 'api/comments'
 
   constructor() { }
 }
