4a58e686cfd9859f8fdfe02f0c88313f58dff832
fathsam82 <fathsam82@gmail.com>
2025-01-10 11:34:36 -0700

    constructed function to getCommentsByPost in comment.service.ts

--- ngPlanty/src/app/services/comment.service.ts
+++ ngPlanty/src/app/services/comment.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { AuthService } from './auth.service';
 import { Comment } from '../models/comment';
+import { catchError, Observable, switchMap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +25,27 @@ export class CommentService {
     return options;
   }
 
-  getComments(){
+  getCommentsByPost(): Observable<Comment[]> {
+    return this.authService.getLoggedInUser().pipe(
+      switchMap((user) => {
+        if(!user){
+          throw new Error('User not logged in');
+        }
+        return this.httpClient
+        .get<Comment[]>(this.url, this.getHttpOptions())
+        .pipe(catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () =>
+              new Error(
+                'CommentService.getCommentByPost(): error retrieving comments'
+              )
+          )
+        }))
+      })
+
+    )
+
 
   }
 }
